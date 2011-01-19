071ac148698f620a5c0686c1da17020075c2eeb7
Carsten Block <mail@carstenblock.org>
2011-01-19 16:27:25 +0100

    Config Updated

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -62,14 +62,17 @@ environments {
 
 }
 
-// log4j configuration
-log4j = {
-  // Example of changing the log pattern for the default console
-  // appender:
-  //
-  //appenders {
-  //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-  //}
+def catalinaBase = System.properties.getProperty('catalina.base')
+if (!catalinaBase) catalinaBase = '.'   // just in case
+def logDirectory = "${catalinaBase}/logs"
+
+// default for all environments
+log4j = { root ->
+  appenders {
+    rollingFile name: 'stdout', file: "${logDirectory}/${appName}.log".toString(), maxFileSize: '100KB'
+    rollingFile name: 'stacktrace', file: "${logDirectory}/${appName}_stack.log".toString(), maxFileSize: '100KB'
+    //console name: 'stdout', layout: pattern(conversionPattern: "%d [%t] %-5p %c %x - %m%n")
+  }
 
   error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
       'org.codehaus.groovy.grails.web.pages', //  GSP
@@ -81,11 +84,36 @@ log4j = {
       'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
       'org.springframework',
       'org.hibernate',
-      'net.sf.ehcache.hibernate'
+      'org.activemq',
+      'grails.app'
+  root.level = org.apache.log4j.Level.ERROR
+}
 
-  warn 'org.mortbay.log'
+// special settings with development env
+environments {
+  development {
+    log4j = { root ->
+      appenders {
+        console name: 'stdout', layout: pattern(conversionPattern: "%d [%t] %-5p %c %x - %m%n")
+      }
+      warn 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+          'org.codehaus.groovy.grails.web.pages', //  GSP
+          'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+          'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+          'org.codehaus.groovy.grails.web.mapping', // URL mapping
+          'org.codehaus.groovy.grails.commons', // core / classloading
+          'org.codehaus.groovy.grails.plugins', // plugins
+          'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+          'org.springframework',
+          'org.hibernate',
+          'org.activemq',
+          'grails.app'
 
-  info 'org.powertac.visualizer'
+      info 'org.powertac.visualizer'
+
+      root.level = org.apache.log4j.Level.INFO
+    }
+  }
 }
 
 // Added by the Joda-Time plugin:
