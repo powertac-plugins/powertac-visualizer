1cb04044479ceb9a72651a745fbb9ac83769359b
Adis Mustedanagic <adis.msg@gmail.com>
2011-06-26 20:13:07 +0200

    Visualizer update.

--- grails-app/domain/org/powertac/visualizer/Agent.groovy
+++ grails-app/domain/org/powertac/visualizer/Agent.groovy
@@ -1,9 +1,13 @@
 package org.powertac.visualizer
 
+/**
+ * This class could extend the org.powertac.common.Broker if necessary, however,
+ * such a thing would probably result in a lot of unused information.
+ */
 class Agent {
     
     String username
-    BigDecimal balance
+    BigDecimal balance = 0.0 //TODO: Check the brokers starting funds
     List cashHistory = []
 
     static constraints = {
--- grails-app/services/org/powertac/visualizer/MessagingService.groovy
+++ grails-app/services/org/powertac/visualizer/MessagingService.groovy
@@ -6,51 +6,62 @@ import org.powertac.common.Competition
 
 class MessagingService implements VisualizationListener, InitializationService {
 
-  static transactional = true
+    static transactional = true
 
-  def visualizationProxyService
+    def visualizationProxyService
   
-  def competitionName
-  def competitionId
-  def brokers
+    def competitionName
+    def competitionId
+    def brokers
+    
+    List agents = []
 
-  @Override
-  void setDefaults() {
-    // Can be empty
-  }
+    @Override
+    void setDefaults() {
+        // Can be empty
+    }
 
-  @Override
-  String initialize(Competition competition, List<String> completedInits) {
-    this.init()
-    return "Visualizer"
-  }
+    @Override
+    String initialize(Competition competition, List<String> completedInits) {
+        this.init()
+        return "Visualizer"
+    }
 
-  /**
-   * Register the visualizer service with the Visualizer proxy
-   */
-  void init() {
-    visualizationProxyService?.registerVisualizationListener(this)
-    log.info ">>> Registering the visualizer with the visualizerProxy"
-  }
-
-  @Override
-  public void receiveMessage(msg) {
-    println ">>> New message"
     /**
-     * Parse the initial message and collect information about brokers (agents)
-     * and other relevant info about the competition
+     * Register the visualizer service with the Visualizer proxy
      */
-    //println ">>> New message"
-    if (msg instanceof Competition) {
-        //println ">>> Competition"
-        competitionName = msg.name
-        competitionId = msg.id
-        brokers = msg.brokers
-        for (broker in brokers) {
-            // TODO: Create an instance of agent for every broker in the message
-            //println broker
+    void init() {
+        visualizationProxyService?.registerVisualizationListener(this)
+        log.info ">>> Registering the visualizer with the visualizerProxy"
+    }
+
+    @Override
+    public void receiveMessage(msg) {
+        println ">>> New message"
+        /**
+         * Parse the initial message and collect information about brokers (agents)
+         * and other relevant info about the competition
+         */
+        //println ">>> New message"
+        if (msg instanceof Competition) {
+            //println ">>> Competition"
+            competitionName = msg.name
+            competitionId = msg.id
+            brokers = msg.brokers
+            for (broker in brokers) {
+                // TODO: Create an instance of agent for every broker in the message
+                //println broker
+                def agent = new Agent(username: broker)
+                agents.add(agent)
+            }
+            
+            println "--- <Brokers> ---"
+            for (agent in agents) {
+                println agent.username
+                println agent.balance
+            }
+            println "--- </Brokers> ---"
         }
     }
-  }
 
 }
